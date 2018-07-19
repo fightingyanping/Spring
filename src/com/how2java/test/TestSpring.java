a4c330e74ab0de339e7402c75a3065afde35fa97
Janetta Yang <fightingyanping@163.com>
2018-07-19 10:30:15 +0800

    使用注解的方式完成注入对象中的效果

--- src/com/how2java/test/TestSpring.java
+++ src/com/how2java/test/TestSpring.java
@@ -9,9 +9,7 @@ public class TestSpring {
  
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "applicationContext.xml" });
- 
         Product p = (Product) context.getBean("p");
- 
         System.out.println(p.getName());
         System.out.println(p.getCategory().getName());
     }
