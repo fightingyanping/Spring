804659fcafeb7181fd082eaae52d8ba1617e67f9
Janetta Yang <fightingyanping@163.com>
2018-07-19 10:22:51 +0800

    对Product对象，注入一个Category对象

--- src/com/how2java/test/TestSpring.java
+++ src/com/how2java/test/TestSpring.java
@@ -1,17 +1,18 @@
 package com.how2java.test;
-
+ 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.how2java.pojo.Category;
-
+ 
+import com.how2java.pojo.Product;
+ 
 public class TestSpring {
-
-	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "applicationContext.xml" });
-
-		Category c = (Category) context.getBean("c");
-
-		System.out.println(c.getName());
-	}
+ 
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "applicationContext.xml" });
+ 
+        Product p = (Product) context.getBean("p");
+ 
+        System.out.println(p.getName());
+        System.out.println(p.getCategory().getName());
+    }
 }
